c639377cd0e5275b17946fb54a69bf5f5cba8964
Ameyabarve123 <barveameya@yahoo.com>
2025-03-28 22:58:13 -0400

    Revamped frontend design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,39 +4,26 @@ import Footer from "./assets/footer";
 import Main from "./assets/Main";
 import ContentCard from "./assets/ContentCard";
 import About from "./pages/About";
+
 import "./App.css";
 
 function App() {
-  const text1 =
-    "With AI becoming an integral part of everyday applications, ensuring its accessibility to everyone is more important than ever. The Heterogeneous Expert with Routing Decisions model is designed to provide a highly accurate, open-source AI solution that is free for anyone to use. By making this model openly available, we aim to foster community collaboration, enabling continuous improvements and advancements that benefit all.";
-  const text2 = 'Just click the "Chat" link above and type in your prompt to get started!';
-
+ 
   return (
     <Router>
       <Navbar />
-
       <Routes>
         <Route
           path="/"
           element={
             <Main>
               <div className="title">
-                <h1>Heterogeneous Experts with Routing Decisions</h1>
+                <img id="titleImg" src="/titleBg.png" alt="Title" />
               </div>
-
+              
               <div className="content">
-                <ContentCard
-                  imageSrc="/placeHolderImg.png"
-                  flip="no"
-                  text="Purpose"
-                  contentText={text1}
-                />
-                <ContentCard
-                  imageSrc="/placeHolderImg.png"
-                  flip="yes"
-                  text="How to Use"
-                  contentText={text2}
-                />
+  
+               
               </div>
             </Main>
           }
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,23 +1,34 @@
 import { useState } from 'react'
 import "./About.css"
 import Main from '../assets/Main'
+import ContentCard from '../assets/ContentCard'
 
 
 function About() {
-
+  const text1 =
+  "With AI becoming an integral part of everyday applications, ensuring its accessibility to everyone is more important than ever. The Heterogeneous Expert with Routing Decisions model is designed to provide a highly accurate, open-source AI solution that is free for anyone to use. By making this model openly available, we aim to foster community collaboration, enabling continuous improvements and advancements that benefit all.";
+  const text2 = 'Just click the "Chat" link above and type in your prompt to get started!';
 
   return (
     <>
       <Main>
-        <div id='missionStatement'>
-          <div id="missionStatementBg">
-            <div id="missionStatementImg">
-              <img className="cardImgMission" src="public/placeHolderImg.png" alt="Content Image" />
-            </div>
-            <div id="missionStatementText">
-              <h1>Mission Statement</h1>
-            </div>
-          </div>
+        <div className="title">
+          <h1>Heterogeneous Experts with Routing Decisions</h1>
+        </div>
+
+        <div className="content">
+          <ContentCard
+            imageSrc="/placeHolderImg.png"
+            flip="no"
+            text="Purpose"
+            contentText={text1}
+          />
+          <ContentCard
+            imageSrc="/placeHolderImg.png"
+            flip="yes"
+            text="How to Use"
+            contentText={text2}
+          />
         </div>
       </Main>
     </>
