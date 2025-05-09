6913090f7f8ed2da5d0dc36a2c46b3818343e233
Samuel Garnett <garnes2@rpi.edu>
2025-05-09 14:19:19 -0400

    grouping method for experts

--- router/Router.py
+++ router/Router.py
@@ -1,35 +1,34 @@
+#Strategy: Use label families instead of just general labels, like make groups of labels for a expert. 
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 
 class Router():
-    def __init__(self, zero_shot_path:str, experts:list):
+    def __init__(self, zero_shot_path:str, experts:dict):
         self.classifier = pipeline("zero-shot-classification", model=zero_shot_path)
         self.expertList = experts
 
     def expertClassification(self, prompt:str):
-        result = self.classifier(prompt, candidate_labels=self.expertList)
+        labels = [item for sublist in self.expertList.values() for item in sublist]
         relevantExperts = list()
-        print(result["scores"])
+
+        result = self.classifier(prompt, candidate_labels=labels)
+        print(result["scores"]) #debug print
+
         for index, score in enumerate(result["scores"]):
             if (score >= 0.2):
-                relevantExperts.append((result["labels"])[index])
-        
+                matchingKeys = [k for k, v in self.expertList.items() if (result["labels"])[index] in v]
+                relevantExperts.append(matchingKeys[0])
+                
         return relevantExperts
 
 
 if __name__ == "__main__":
 
-    # classifier = pipeline("zero-shot-classification", model="/gpfs/u/home/ARUS/ARUSgrsm/scratch/HFModels/models--facebook--bart-large-mnli/snapshots/d7645e127eaf1aefc7862fd59a17a5aa8558b8ce")
-
-    # prompt = "The anti-diuretic hormone controls the regulation of urea in the human body."
-
-    # labels = ["Biology", "Anatomy", "Physics", "Math"]
-
-    # result = classifier(prompt, candidate_labels=labels)
-
-    # print(result["labels"])
-    # print(result["scores"])
-
-    experts = ["Biology", "Computer Science", "Physics", "Math"]
+    experts = {"Biology" : ["biology", "medicine", "biological processes", "anatomy", "cells", "cell structure", "biotechnology", "genes", "gene editing"],
+               "Computer Science" : ["Computer Science", "Python", "C++", "Java", "C#", "Programming"], 
+               "Physics" : ["Physics"],
+               "Math" : ["Math"]
+               }
+    
     routingAgent = Router("/gpfs/u/home/ARUS/ARUSgrsm/scratch/HFModels/models--facebook--bart-large-mnli/snapshots/d7645e127eaf1aefc7862fd59a17a5aa8558b8ce", experts)
     relevantExperts = routingAgent.expertClassification("Can you make me a python program that uses computer vision to identify cancer cells in an electron-microscope image?")
     print(relevantExperts)
