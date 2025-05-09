eb99c95f13e6efc2eadd54c3e33afa3aea1d2c2a
Samuel Garnett <garnes2@rpi.edu>
2025-05-09 15:57:10 -0400

    lowing threshold

--- router/Router.py
+++ router/Router.py
@@ -14,7 +14,7 @@ class Router():
         print(result["scores"]) #debug print
         print(result["labels"])
         for index, score in enumerate(result["scores"]):
-            if (score >= 0.1):
+            if (score >= 0.05):
                 print(result["labels"][index])
                 matchingKeys = [k for k, v in self.expertList.items() if (result["labels"])[index] in v]
                 if matchingKeys[0] not in relevantExperts.keys():
